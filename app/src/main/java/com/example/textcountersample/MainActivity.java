ea175043f4796c56fb7e9e6243a91c03040b45dd
EditaJur <82542281+EditaJur@users.noreply.github.com>
2021-05-06 20:41:10 +0300

    initial commit

--- app/src/main/java/com/example/textcountersample/MainActivity.java
+++ app/src/main/java/com/example/textcountersample/MainActivity.java
@@ -35,14 +35,12 @@ public class MainActivity extends AppCompatActivity {
         EditText edUserInput = findViewById(R.id.edUserInput);
         TextView tvOutput = findViewById(R.id.tvOutput);
         String userInputText = edUserInput.getText().toString();
-        String words[]=userInputText.split( "\\s");
-
-        String selection = this.ddSelection.getSelectedItem().toString();
+        String words[]=userInputText.split("\\s");
+           String selection = this.ddSelection.getSelectedItem().toString();
         if(selection.equalsIgnoreCase("Chars")){
             int count = getCharsCount(userInputText);
             tvOutput.setText(String.valueOf(count));
         }
-
         else{
             tvOutput.setText(String.valueOf(words.length));
         }
